36312eef11a413af8d60ad6a06c91b6d1c7bf62a
Jack Newton <jnewton@avvo.com>
2018-04-05 15:02:16 -0700

    Show with comment how you could require a webpack config per environment.

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,6 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
 module.exports = (env) => {
+  // const envConfig = require(`webpack.${env.mode}`);
   return webpackMerge({mode: env.mode}, commonConfig);
 };
